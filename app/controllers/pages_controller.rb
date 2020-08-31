7ffc2a7be84f41db97a126f8bb38454b9d99b019
Lily Tan <lilyt2995@gmail.com>
2020-08-31 13:37:30 +1000

    Created application to ask coach questions and styled using SASS

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+    def ask
+      
+    end
+
+    def answer
+        if params[:question]
+            @question = params[:question]
+            if @question.downcase == "i am going to work right now!"
+                @answer = ""
+            elsif @question.end_with?("?")
+                @answer = "Silly question, get dressed and go to work!"
+            else
+                @answer = "I don't care, get dressed and go to work!"
+            end
+        end
+    end
+end
+
